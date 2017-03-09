4622d44c758b8366b605cfdc61519f9846258699
Henry Tseng <henrybear327@gmail.com>
2017-03-10 00:56:08 +0800

    Formatted main.c

--- ass2/main.c
+++ ass2/main.c
@@ -5,18 +5,19 @@ typedef long long ll;
 int main()
 {
     ll data[100];
-	// base case
+    // base case
     data[0] = 0;
     data[1] = 1;
-	
-	// use formula f[i] = f[i - 1] + f[i - 2] to compute the ith fibonacci number
+
+    // use formula f[i] = f[i - 1] + f[i - 2] to compute the ith fibonacci number
     for (int i = 2; i <= 90; i++) {
         data[i] = data[i - 1] + data[i - 2];
     }
-	
-	// print the results out on the screen
+
+    // print the results out on the screen
     for (int i = 0; i < 90; i++)
-        printf("%3d = %18lld\n", i + 1, data[i]); // align the output for better result displaying
+        printf("%3d = %18lld\n", i + 1,
+               data[i]); // align the output for better result displaying
 
     return 0;
 }
