8ff3cf1276d617858c76d623c0fb971c1fd023dc
Henry Tseng <henrybear327@gmail.com>
2017-04-08 16:19:47 +0800

    Wrap up ass4

--- ass4/main.c
+++ ass4/main.c
@@ -16,11 +16,12 @@ const int MAX_N = 10000;
 #define GREEN "\033[0;32;32m"
 #define CYAN "\033[0;36m"
 
-#define DEBUG 1
+#define DEBUG 0
 
 int main()
 {
 	char inp[MAX_N];
+	printf(CYAN "Please enter a student name and 5 grades\n" NONE);
 	while(fgets(inp, MAX_N, stdin) != NULL) {
 		// read input
 #if DEBUG == 1
@@ -51,7 +52,7 @@ int main()
 #endif
 
 		// open file
-		printf("Opening file....\n");
+		printf(GREEN "Opening file....\n" NONE);
 		int outputFilefd = open("grade", O_WRONLY | O_CREAT | O_APPEND, S_IRUSR| S_IWUSR);
 		if(outputFilefd == -1) {
 			perror("output file open() error");
@@ -59,24 +60,27 @@ int main()
 		}
 
 		// acquire lock
-		printf("Acquiring file lock....\n");
+		printf(GREEN "Trying to acquire file lock....\n" NONE);
 		int acquireRet = flock(outputFilefd, LOCK_EX);
-		printf(GREEN "%d\n" NONE, acquireRet);
 		if(acquireRet == -1) {
 			perror("acquire lock flock() error");
 			return EXIT_FAILURE;
 		}
+		printf(GREEN "File lock has been acquired!\n" NONE);
 		
 		// write data
-		printf("Writing to file....\n");
+		printf(GREEN "Writing to file....\n" NONE);
 		int writingByte = write(outputFilefd, toWrite, sizeof(toWrite));
 		if(writingByte == -1) {
 			perror("write() error");
 			return EXIT_FAILURE;
 		}
+		
+		printf("Wait for 10 seconds before releasing the lock...\n");
+		sleep(10);
 
 		// release lock
-		printf("Releasing lock....\n");
+		printf(GREEN "Releasing lock....\n" NONE);
 		int releaseRet = flock(outputFilefd, LOCK_UN);
 		if(releaseRet == -1) {
 			perror("release lock flock() error");
@@ -84,11 +88,14 @@ int main()
 		}
 		
 		// close file
-		printf("Closing file....\n");
+		printf(GREEN "Closing file....\n" NONE);
 		if(close(outputFilefd)) {
 			perror("close() error");
 			return EXIT_FAILURE;
 		}	
+
+		// print message
+		printf(CYAN "Please enter a student name and 5 grades\n" NONE);
 	}
 
 	return 0;
